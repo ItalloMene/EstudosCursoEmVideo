654eec69de30813451c24c768a923bd698860d18
ItalloMene <italloborgessilva22@gmail.com>
2025-05-15 16:25:32 -0300

    estudos JavaScript - Estruturas de Repetição (parte 1) - Curso em Vídeo

---
+++ JavaScript/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 20)
+
+/*
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
---
+++ JavaScript/aula13/node_4d6e11a26191b.tmp.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+} while (c <= 20)
+
+/*
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
